8a39296de02b34dc354982775462516adee319b5
Larz Conwell <larzconwell@gmail.com>
2022-02-18 22:21:04 -0500

    Add comments explaining some logic

--- kernel.c
+++ kernel.c
@@ -3,11 +3,12 @@
 
 typedef unsigned char uint8_t;
 
-// Framebuffer mapped address
 uint8_t *fb = (uint8_t *)0x000B8000;
 
 void fb_write_cell(uint8_t cell, uint8_t val) {
     fb[cell] = val;
+    // | 7 6 5 4 | 3 2 1 0 |
+    // | fg      | bg      |
     fb[cell + 1] = ((fg & 0x0F) << 4) | (bg & 0x0F);
 }
 
--- loader.s
+++ loader.s
@@ -2,6 +2,7 @@ global loader
 extern sum
 extern fb_write_cell
 
+; Multiboot data
 MAGIC equ 0x1BADB002
 FLAGS equ 0x0
 CHECKSUM equ -MAGIC
@@ -21,12 +22,17 @@ align 4
 section .text:
 loader:
     mov esp, kernel_stack + STACK_SIZE
+
     push byte 2
     push byte 1
     call sum
+
+    ; Convert result to ASCII
     add eax, 48
+
     push eax
     push byte 0
     call fb_write_cell
+
 .loop:
     jmp .loop
